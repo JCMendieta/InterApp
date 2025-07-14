f6f25531eed96d0af98f28cebdd9e3a9960ebe7a
camilo <juank.yoh.117@gmail.com>
2025-07-14 15:26:23 -0500

    Initial commit

---
+++ InterApp/InterApp/InterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterAppApp.swift
+//  InterApp
+//
+//  Created by Juan Camilo Mendieta Hernández on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct InterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
